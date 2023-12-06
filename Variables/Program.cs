a890bd0dd7f3458b6e1953983b16034ebfec2546
A252426 <A252426@LSBSA-BC86HW3>
2023-12-06 06:37:46 +0200

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variable
+            string firstname;
+            int age;
+
+            //initialisation 
+            firstname = "Inathi";
+            age = 12;
+
+            //what will be on console
+            Console.WriteLine("Hi " + firstname + " you are " +  age + "years old");
+             
+            firstname = "Noma";
+            age = 75;
+
+            Console.WriteLine("Hallo " + firstname + " you are " + age + "years old");
+
+            string Lastname = "Thomas";
+            Console.WriteLine("Hallo I am " + firstname + " " + Lastname + " a " + age + "year old strong women");
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
